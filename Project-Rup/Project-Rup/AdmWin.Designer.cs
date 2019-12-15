147d7a663dd9303f6c9238f6f65ca99f754a2d14
Klaudia Marciniak <klamar7@st.amu.edu.pl>
2019-12-15 02:56:17 +0100

    [Admin] Displaying pending lesson plans

--- Project-Rup/Project-Rup/AdmWin.Designer.cs
+++ Project-Rup/Project-Rup/AdmWin.Designer.cs
@@ -29,25 +29,95 @@
         private void InitializeComponent()
         {
             this.loginLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabela1 = new System.Windows.Forms.DataGridView();
+            this.idStudentColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sheduleStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.accept = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.reject = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tabela1)).BeginInit();
             this.SuspendLayout();
             // 
             // loginLabel
             // 
             this.loginLabel.AutoSize = true;
-            this.loginLabel.Location = new System.Drawing.Point(13, 13);
+            this.loginLabel.Location = new System.Drawing.Point(12, 10);
             this.loginLabel.Name = "loginLabel";
-            this.loginLabel.Size = new System.Drawing.Size(104, 20);
+            this.loginLabel.Size = new System.Drawing.Size(93, 17);
             this.loginLabel.TabIndex = 0;
             this.loginLabel.Text = "Zalogowano :";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(413, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // tabela1
+            // 
+            this.tabela1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabela1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idStudentColumn,
+            this.sheduleStatus,
+            this.accept,
+            this.reject});
+            this.tabela1.Location = new System.Drawing.Point(41, 112);
+            this.tabela1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabela1.Name = "tabela1";
+            this.tabela1.RowHeadersWidth = 51;
+            this.tabela1.Size = new System.Drawing.Size(787, 288);
+            this.tabela1.TabIndex = 2;
+            
+            // 
+            // idStudentColumn
+            // 
+            this.idStudentColumn.HeaderText = "Imię i nazwisko";
+            this.idStudentColumn.MinimumWidth = 6;
+            this.idStudentColumn.Name = "idStudentColumn";
+            this.idStudentColumn.ReadOnly = true;
+            this.idStudentColumn.Width = 125;
+            // 
+            // sheduleStatus
+            // 
+            this.sheduleStatus.HeaderText = "Status planu";
+            this.sheduleStatus.MinimumWidth = 6;
+            this.sheduleStatus.Name = "sheduleStatus";
+            this.sheduleStatus.Width = 125;
+            // 
+            // accept
+            // 
+            this.accept.HeaderText = "Akceptacja";
+            this.accept.MinimumWidth = 6;
+            this.accept.Name = "accept";
+            this.accept.Text = "Zaakceptuj";
+            this.accept.Visible = true;
+            this.accept.Width = 125;
+            //
+            // reject
+            //
+            this.reject.HeaderText = "Odrzucenie";
+            this.reject.MinimumWidth = 6;
+            this.reject.Name = "reject";
+            this.reject.Text = "Odrzuć";
+            this.reject.Visible = true;
+            this.reject.Width = 125;
+            // 
             // AdmWin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(929, 505);
+            this.Controls.Add(this.tabela1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.loginLabel);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AdmWin";
             this.Text = "AdmWin";
+            ((System.ComponentModel.ISupportInitialize)(this.tabela1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +126,11 @@
         #endregion
 
         private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView tabela1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idStudentColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sheduleStatus;
+        private System.Windows.Forms.DataGridViewButtonColumn accept;
+        private System.Windows.Forms.DataGridViewButtonColumn reject;
     }
 }
--- Project-Rup/Project-Rup/AdmWin.cs
+++ Project-Rup/Project-Rup/AdmWin.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,6 +18,87 @@ namespace Project_Rup
         {
             InitializeComponent();
             loginLabel.Text = "Zalogowano administratora: " + login + " " + password;
+            label1.Text = "Oczekujące plany";
+
+            string connetionString, sql = "";
+            SqlConnection DBconnect;
+            connetionString = @"Data Source=mssql-2017.labs.wmi.amu.edu.pl;Initial Catalog=s434903_inzopr2019z;User ID=s444513;Password=Gxrbqfvw7L";
+            DBconnect = new SqlConnection(connetionString);
+            DBconnect.Open();
+            sql = "SELECT * FROM Plan_Zajec WHERE Status LIKE 'Oczekujący'";
+            SqlCommand command = new SqlCommand(sql, DBconnect);
+            SqlDataReader reader = command.ExecuteReader();
+
+            int counter = 0;
+
+            while (reader.Read())
+            {
+
+                counter++;
+                //wyczytuje ile jest wierszy w stworzonej tabeli
+            }
+            // Console.WriteLine("Ilosc wierszy: " +counter); 
+            DBconnect = new SqlConnection(connetionString);
+            DBconnect.Open();
+            sql = "SELECT * FROM Plan_Zajec WHERE Status LIKE 'Oczekujący'";
+            SqlCommand command1 = new SqlCommand(sql, DBconnect);
+            SqlDataReader reader1 = command1.ExecuteReader();
+            ArrayList uzyteid = new ArrayList();
+            if (counter > 0)
+            //jesli jest wiecej niz 0 wierszy, zaczyna czytac
+            {
+                for (int i = 1; i <= counter; i++)
+                {
+
+                    reader1.Read();
+                    string id_studenta;
+                    id_studenta = reader1.GetValue(1).ToString();
+
+                    // Console.WriteLine("id studenta: " + id_studenta);
+
+
+                    //  Console.WriteLine("wielkosc listy: " + uzyteid.Count);
+
+
+
+
+                    if (!uzyteid.Contains(id_studenta))
+                    {
+                        uzyteid.Add(id_studenta);
+                        // Console.WriteLine("wielkosc listy po dodaniu: " + uzyteid.Count);
+                        DBconnect = new SqlConnection(connetionString);
+                        DBconnect.Open();
+                        sql = "Select * from Studenci where id=" + id_studenta;
+                        SqlCommand command2 = new SqlCommand(sql, DBconnect);
+                        SqlDataReader reader2 = command2.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            reader2.Read();
+                            string name;
+                            string surname;
+                            name = reader2.GetValue(1).ToString();
+
+                            surname = reader2.GetValue(2).ToString();
+                            //Console.WriteLine(name + " " + surname);
+
+                            string status = "oczekujący";
+                            tabela1.Rows.Add(name + " " + surname, status, "Zaakceptuj", "Odrzuć");
+
+
+                        }
+
+                    }
+
+                }
+            }
+            
         }
+        
+
     }
+    private void accept_Click(object sender, EventArgs e)
+        {
+
+
+        }
 }
